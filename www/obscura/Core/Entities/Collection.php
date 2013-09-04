f855a2524281ce8fa86a37ef987ac623bc99216d
Erik J. Olson <github@arych.com>
2013-09-04 14:22:30 -0400

    Build out Collection()

---
+++ www/obscura/Core/Entities/Collection.php
@@ -0,0 +1,110 @@
+<?php
+	/**
+	 *	Obscura Photo Management System
+	 *	http://www.github.com/arychj/obscura
+	 *	©2013 Erik J. Olson
+	 *
+	 *	-----------------------------------------------------------------------------
+	 *	"THE BEER-WARE LICENSE" (Revision 42):
+	 *	<erikjolson@arych.com> wrote this file. As long as you retain this notice you
+	 *	can do whatever you want with this stuff. If we meet some day, and you think
+	 *	this stuff is worth it, you can buy me a beer in return. Erik J. Olson.
+	 *	-----------------------------------------------------------------------------
+	 *
+	 *	Obscura.Core.Entities.Collection
+	 *	<description>
+	 *
+	 *	@changelog
+	 *	2013.09.04
+	 *		Created
+	 */
+
+	PackageManager::Import('Core.Common.Database');
+
+	class Collection extends Entity {
+		private $loaded = false;
+
+		private $cover, $thumbnail;
+		private $albums;
+
+		/*** accessors ***/
+
+		protected function get_Cover(){
+			$this->Load();
+			return $this->cover;
+		}
+
+		protected function get_Thumbnail(){
+			$this->Load();
+			return $this->thumbnail;
+		}
+
+		protected function get_Photos(){
+			$this->Load();
+			return $this->photos;
+		}
+
+		/*** end accessors ***/
+
+		protected function __construct($id, $loadImmediately = false, $entity = null){
+			if($entity != null)
+				parent::__construct($id, false, $entity);
+			else{
+				$this->id = $id;
+				if($loadImmediately)
+					$this->Load();	
+			}
+		}
+
+		public function ToXml(){
+
+		}
+		
+		private function Load(){
+			if(!$this->loaded){
+				$sth = Database::Prepare("SELECT id_cover, id_thumbnail FROM tblCollections where id_entity = :id");
+				$sth->bindValue('id', $this->Id, PDO::PARAM_INT);
+				$sth->execute();
+
+				if(($details = $sth->fetch()) != null){
+					$this->image = Image::Retrieve($details->id_cover);
+					$this->thumbnail = Image::Retrieve($details->id_thumbnail);
+
+					$this->loaded = true;
+				}
+				else{
+					throw new EntityException("Invalid Collection Id: {$this->id}");
+				}
+			}
+		}
+
+		public static function Create($title, $description, $cover, $thumbnail){
+			if(get_class($cover) != 'Image')
+				throw new InvalidArgumentException();	
+			elseif(get_class($thumbnail) != 'Image')
+				throw new InvalidArgumentException();
+
+			$entity = Entity::Create(EntityTypes::Photo, $title, $description);
+			
+			$sth = Database::Prepare("INSERT INTO tblCollections (id_entity, id_cover, id_thumbnail) VALUES (:id_entity, :id_cover, :id_thumbnail)");
+			$sth->bindValue('id_entity', $entity->Id, PDO::PARAM_INT);
+			$sth->bindValue('id_cover', $cover->Id, PDO::PARAM_INT);
+			$sth->bindValue('id_thumbnail', $thumbnail->Id, PDO::PARAM_INT);
+
+			$collection = new Collection($entity->Id, false, $entity);
+			$collection->cover = $cover;
+			$collection->thumbnail = $thumbnail;
+
+			return $collection;
+		}
+
+		public static function Retrieve($id, $loadImmediately = false){
+			return new Collection($id, $loadImmediately);
+		}
+
+		public static function All(){
+
+		}
+	}
+
+?>
