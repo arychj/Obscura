e2fef11df45c178792a44fad12703c62c9f57865
Erik J. Olson <github@arych.com>
2013-09-09 12:17:09 -0400

    Added missing files

---
+++ www/obscura/Core/Entities/EntityTypes.php
@@ -0,0 +1,32 @@
+<?php
+	/**
+	 *	Obscura Photo Management System
+	 *	http://www.github.com/arychj/obscura
+	 *	©2013 Erik J. Olson
+	 *
+	 *	-----------------------------------------------------------------------------
+	 *	"THE BEER-WARE LICENSE" (Revision 42):
+	 *	<erikjolson@arych.com> wrote this file. As long as you retain this notice you
+	 *	can do whatever you want with this stuff. If we meet some day, and you think
+	 *	this stuff is worth it, you can buy me a beer in return. Erik J. Olson.
+	 *	-----------------------------------------------------------------------------
+	 *
+	 *	Obscura.Core.Entities.Entity
+	 *	Entity Types enum
+	 *
+	 *	@changelog
+	 *	2013.09.09
+	 *		Created
+	 */
+
+	class EntityTypes {
+		const Entity = "Entity";
+		const Image = "Image";
+		const Photo = "Photo";
+		const Album = "Album";
+		const Collection = "Collection";
+		const Journal = "Journal";
+		const Video = "Video";
+	}
+
+?>
---
+++ www/obscura/Core/Entities/Shelf.php
@@ -0,0 +1,37 @@
+<?php
+	/**
+	 *	Obscura Photo Management System
+	 *	http://www.github.com/arychj/obscura
+	 *	©2013 Erik J. Olson
+	 *
+	 *	-----------------------------------------------------------------------------
+	 *	"THE BEER-WARE LICENSE" (Revision 42):
+	 *	<erikjolson@arych.com> wrote this file. As long as you retain this notice you
+	 *	can do whatever you want with this stuff. If we meet some day, and you think
+	 *	this stuff is worth it, you can buy me a beer in return. Erik J. Olson.
+	 *	-----------------------------------------------------------------------------
+	 *
+	 *	Obscura.Core.Entities.Shelf
+	 *	<Description>
+	 *
+	 *	@changelog
+	 *	2013.09.04
+	 *		Created
+	 */
+
+	class ShelfName {
+		const Entity = "Entity";
+		const EntityCollection = "EntityCollection";
+	}
+
+	class Shelf {
+		
+		public static function Shelve($shelf, $name, $object){
+			
+		}
+
+		public static function Unshelve($shelf, $name){
+			return null;
+		}
+
+?>
